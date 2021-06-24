14661c34d62a54369ebd473dc82060017c896a08
shy540 <84746209+shy540@users.noreply.github.com>
2021-06-24 12:02:43 -0600

    Clustering Algorithms with Methods

---
+++ DBSCAN_clustering_Methods.py
@@ -0,0 +1,24 @@
+from sklearn.cluster import DBSCAN
+import pandas as pd
+
+
+def getDBSCANClusterLabels(data, max_distance, min_num_samples):
+    # Generate DBSCAN cluster
+    cluster_DBSCAN = DBSCAN(eps=max_distance, min_samples=min_num_samples).fit(data)
+    # Get DBSCAN cluster labels 
+    cluster_DBSCAN_Labels = cluster_DBSCAN.labels_
+    return cluster_DBSCAN_Labels
+
+def main():
+    # Read in data
+    data = pd.read_csv('nci1.csv')
+    data = data.drop(['type'], axis= 1)  
+    # Select eps and min number of samples for DBSCAN
+    eps = 1.00275
+    min_samples = 2
+    # Get results for DBSCAN clustering algorithm
+    cluster_DBSCAN_Labels = getDBSCANClusterLabels(data, eps, min_samples)
+    # When eps is chosen too small, most data will not be clustered at all --> -1 means labeled for noise
+    print("DBSCAN Clustering Labels:", cluster_DBSCAN_Labels)
+    
+main()
---
+++ NMI_clustering_Methods.py
@@ -0,0 +1,50 @@
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+import pandas as pd
+
+def scoreNMIforDBSCAN(data, y_pred):
+    # Run DBSCAN clustering algorithm
+    cluster_DBSCAN = DBSCAN(eps=1.00275, min_samples=2).fit(data)
+    # Label output for each cluster
+    y_true_DBSCAN = cluster_DBSCAN.labels_
+    # Run NMI on cluster array
+    nmiScore_DBSCAN = normalized_mutual_info_score(y_true_DBSCAN, y_pred)
+    return nmiScore_DBSCAN
+    
+def scoreNMIforKmeans(data, y_pred):
+    # Run K means clustering algorithm
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(data)
+    # Label output for each cluster
+    y_true_kmeans = kmeans.labels_
+    # Run NMI on cluster array
+    nmiScore_kmeans = normalized_mutual_info_score(y_true_kmeans, y_pred)
+    return nmiScore_kmeans
+
+def scoreNMIforAgglomerativeClustering(data, y_pred):
+    # Run Agglomerative clustering algorithm
+    agglomerativeCluster = AgglomerativeClustering(n_clusters=5).fit(data)
+    # Label output for each cluster
+    y_true_agglomerative = agglomerativeCluster.labels_
+    # Run NMI on cluster array
+    nmiScore_agglomerative = normalized_mutual_info_score(y_true_agglomerative, y_pred)
+    return nmiScore_agglomerative
+
+def main():
+    # Read in data
+    data = pd.read_csv('nci1.csv')
+    # Array with predicted output
+    y_pred = [0,0,3,3,4,4,4,0,1,1,1,2,2,2,3]
+    
+    # Get results for NMI on three clustering algorithms
+    nmiScore_DBSCAN = scoreNMIforDBSCAN(data, y_pred)
+    nmiScore_agglomerative = scoreNMIforKmeans(data, y_pred)
+    nmiScore_kmeans = scoreNMIforAgglomerativeClustering(data, y_pred)
+    
+    # Results between 0 (no mutual information) and 1 (perfect correlation)
+    print("NMI Score for DBSCAN:", nmiScore_DBSCAN)
+    print("NMI Score for K Means:", nmiScore_kmeans)
+    print("NMI Score for Agglomerative Clustering:", nmiScore_agglomerative)
+
+main()
